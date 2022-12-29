7893c80b810c21e5269f9a49ae767b744bc0eb06
Aleksey <golovnka14@yandex.ru>
2022-12-30 02:08:04 +0400

    add new tasks

---
+++ task50/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+
+Console.Write("Введите число строк массива : ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число столбцов массива : ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, colums, min, max);
+
+PrintArray(array);
+
+void PrintArray(int[,] arr) {
+   for (int i = 0; i < arr.GetLength(0); i++)
+   {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] >= 0 ? $"  {arr[i,j]}" : $" {arr[i,j]}");
+        }    
+        Console.WriteLine();
+   }
+}
+
+int[,] FillArray(int rowArray, int colArray, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rowArray, colArray];
+    for (int i = 0; i < rowArray; i++)
+        for (int j = 0; j < colArray; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    return resultArray;
+}
+Console.WriteLine("Введите номер строки: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер столбца: ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+if (i < array.GetLength(0) && j < array.GetLength(1)) Console.WriteLine(array[i,j]);
+else Console.WriteLine($"{i-1},{j-1} -> такого числа в массиве нет");
---
+++ task52/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] arr)
+{
+    Console.WriteLine("----------------------------");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] >= 0 ? $"  {arr[i, j]}" : $" {arr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+ PrintArray(array);
+
+Console.WriteLine("---------------------------");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{ sum / array.GetLength(0)} ;");
+}
